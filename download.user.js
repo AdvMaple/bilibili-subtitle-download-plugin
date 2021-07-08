1ce862f89c4c3b6c15ad3c77bb0f706576eac89c
AdvMaple <46006210+AdvMaple@users.noreply.github.com>
2021-07-09 02:03:50 +0700

    a

--- download.user.js
+++ download.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         New Userscript
+// @name         biliintl download
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
