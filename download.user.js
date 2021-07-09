bae17d417209cdd0c14080b341fe9d421d074742
AdvMaple <46006210+AdvMaple@users.noreply.github.com>
2021-07-09 14:38:07 +0700

    a

--- download.user.js
+++ download.user.js
@@ -7,10 +7,14 @@
 // @icon         https://www.google.com/s2/favicons?domain=biliintl.com
 // @updateURL https://github.com/AdvMaple/biliintl_subtitle_download_plugin/raw/main/download.user.js
 // @grant       GM_addStyle
+// @require http://code.jquery.com/jquery-3.6.0.min.js
 
 // ==/UserScript==
-
 (function () {
+  var sort_by_name = function (a, b) {
+    return a.innerHTML.toLowerCase().localeCompare(b.innerHTML.toLowerCase());
+  };
+
   var zNode = document.createElement("div");
   zNode.innerHTML = '<button id="myButton" type="button"> Click me </button>';
   zNode.setAttribute("id", "myContainer");
@@ -21,7 +25,6 @@
     .addEventListener("click", ButtonClickAction, false);
 
   function ButtonClickAction(zEvent) {
-    // setTimeout(()=>{},300)
     try {
       var div_content =
         document.getElementsByClassName("video-info__title")[0].innerText;
@@ -46,6 +49,7 @@
       .then((data) => (x = data))
       .then(() => {
         epList = x.data.episodes;
+
         epList.map((x) => {
           const { title, ep_id } = x;
           console.log(title, ep_id);
@@ -54,78 +58,58 @@
             `https://api.biliintl.com/intl/gateway/m/subtitle?ep_id=${ep_id}`
           )
             .then((r) => r.json())
-            .then(
-              (d) => {
-                const { data } = d;
-                len = data.subtitles.length;
-                for (let i = 0; i < len; i++) {
-                  if (data.subtitles[i].key == "vi") {
-                    // console.log(data.subtitles[i].url);
-                    ep_sub_url = data.subtitles[i].url;
-                    fetch(ep_sub_url)
-                      .then((r) => r.json())
-                      .then((d) => {
-                        var blob = new Blob([JSON.stringify(d)], {
-                          type: "application/json",
-                        });
-                        var url_from_blob = URL.createObjectURL(blob);
-
-                        var a = document.createElement("a");
-                        a.download = `${div_content} ep ${title}.json`;
-                        a.textContent = title + " ";
-                        a.href = url_from_blob;
-                        document.getElementById("myContainer").appendChild(a);
+            .then((d) => {
+              const { data } = d;
+              // console.log(data);
+              len = data.subtitles.length;
+              for (let i = 0; i < len; i++) {
+                if (data.subtitles[i].key == "vi") {
+                  ep_sub_url = data.subtitles[i].url;
+                  fetch(ep_sub_url)
+                    .then((r) => r.json())
+                    .then((d) => {
+                      var blob = new Blob([JSON.stringify(d)], {
+                        type: "application/json",
                       });
-                  }
+                      var url_from_blob = URL.createObjectURL(blob);
+
+                      var a = document.createElement("a");
+                      a.download = `${div_content} ep ${title}.json`;
+                      a.textContent = " " + title;
+                      a.href = url_from_blob;
+                      document.getElementById("myContainer").appendChild(a);
+                    });
                 }
               }
-              // console.log(data.subtitles);
-
-              // var a = document.createElement("a");
-              // a.textContent = title + " ";
-              // a.href = "http://google.com";
-              // document.getElementById("myContainer").appendChild(a);
-            );
+            });
         });
-      });
+      })
+      .then(() => {
+        var zNode = document.createElement("div");
+        zNode.innerHTML =
+          '<button id="mySortBtn" type="button"> sort </button>';
+        zNode.setAttribute("id", document.getElementById("myContainer"));
+        document.getElementById("myContainer").appendChild(zNode);
 
-    //   var zNode = document.createElement("a");
-    //   a.textContent = title;
-    //   zNode.innerHTML = "The button was clicked";
-    //   document.getElementById("myContainer").appendChild(zNode);
+        document
+          .getElementById("mySortBtn")
+          .addEventListener("click", ButtonSortClick, false);
+
+        function ButtonSortClick(zEvent) {
+          var sort_by_num = function (a, b) {
+            return a.innerText - b.innerText;
+          };
+
+          var list = $("#myContainer > a").get();
+          list.sort(sort_by_num);
+          for (var i = 0; i < list.length; i++) {
+            list[i].parentNode.appendChild(list[i]);
+          }
+        }
+      });
   }
 
   console.log("Hello");
-
-  // var url = window.location.href;
-  // var id = url.match(/\d+/g);
-  // var series_id = id[0];
-  // var ep_id = id[1];
-
-  // console.log(url);
-  // console.log("series_id", series_id);
-  // console.log("ep_id", ep_id);
-
-  // var x;
-  // fetch(
-  //   `https://api.biliintl.com/intl/gateway/web/view/ogv_collection?s_locale=vi&season_id=${series_id}`
-  // )
-  //   .then((r) => r.json())
-  //   .then((data) => (x = data))
-  //   .then(() => {
-  //     epList = x.data.episodes;
-  //     epList.map((x) => {
-  //       const { title, ep_id } = x;
-  //       console.log(title, ep_id);
-
-  //       fetch(`https://api.biliintl.com/intl/gateway/m/subtitle?ep_id=${ep_id}`)
-  //         .then((r) => r.json())
-  //         .then((d) => {
-  //           const { data } = d;
-  //           console.log(data.subtitles);
-  //         });
-  //     });
-  //   });
 
   // Style newly added button
   GM_addStyle(`
