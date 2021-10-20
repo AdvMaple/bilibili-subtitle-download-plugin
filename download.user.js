3dfdc9eb520808a907b277145b17f2f46b479725
AdvMaple <46006210+AdvMaple@users.noreply.github.com>
2021-10-20 08:55:24 +0700

    Because I'm stupid

--- download.user.js
+++ download.user.js
@@ -3,7 +3,7 @@
 // @version      0.4.1
 // @description  download json subtitle from biliintl
 // @author       AdvMaple
-// @match        *
+// @match        www.bili*
 // @include      /www.bili*/
 // @icon         https://www.google.com/s2/favicons?domain=biliintl.com
 // @updateURL    https://github.com/AdvMaple/biliintl_subtitle_download_plugin/raw/main/download.user.js
