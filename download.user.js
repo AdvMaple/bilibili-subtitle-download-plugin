fcc27dee6c3a6f4f303a13ab8c846cfb59022876
AdvMaple <46006210+AdvMaple@users.noreply.github.com>
2021-10-20 08:25:02 +0700

    Change match to include for changing url

--- download.user.js
+++ download.user.js
@@ -3,7 +3,7 @@
 // @version      0.3.2
 // @description  download json subtitle from biliintl
 // @author       AdvMaple
-// @match        *://www.biliintl.com/*
+// @match        /www.bili*/
 // @icon         https://www.google.com/s2/favicons?domain=biliintl.com
 // @updateURL    https://github.com/AdvMaple/biliintl_subtitle_download_plugin/raw/main/download.user.js
 // @grant        GM_addStyle
